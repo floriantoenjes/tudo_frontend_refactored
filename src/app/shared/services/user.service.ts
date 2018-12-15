fa55211bd85ccb9e0593ace1f48a9a271b5fae00
Florian Tönjes <skatepainter@yahoo.de>
2018-12-15 18:51:58 +0100

    Added getUsers function

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { RestClientService } from './rest-client.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: RestClientService) { }
+  constructor(
+    private http: RestClientService
+  ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.getRestEntities<User>('/users', 'users');
+  }
 }
